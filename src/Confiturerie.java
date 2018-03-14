e34cc7d23d5af7dff19c7bdc65a325a315240404
nla23 <37373877+nla23@users.noreply.github.com>
2018-03-14 10:23:30 -0400

    Création du projet

---
+++ src/Confiturerie.java
@@ -0,0 +1,17 @@
+/*
+ * Equipe 1 - Projet Confiturerie - GLO-3003
+ * 
+ * Stephane Lalancette 		- 
+ * Francis Charest 	   		- 
+ * Felix Veillette-Potvin 	-
+ * Nicolas Lauzon			- 111 101 145
+ */
+
+public class Confiturerie {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
