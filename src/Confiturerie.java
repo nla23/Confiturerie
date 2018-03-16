e5f11d8776cb3fbfae26d7f627aea8fc2ee56850
Stephane.Lalancette <Stephane.Lalancette@L0446.spe.cginet>
2018-03-16 12:28:19 -0400

    Ajout de Matricule

--- src/Confiturerie.java
+++ src/Confiturerie.java
@@ -1,17 +1,19 @@
 /*
  * Equipe 1 - Projet Confiturerie - GLO-3003
  * 
- * Stephane Lalancette 		- 
+ * Stephane Lalancette 		- 908 279 903 TEST
  * Francis Charest 	   		- 905 179 619
  * Felix Veillette-Potvin 	-
  * Nicolas Lauzon			- 111 101 145
  */
 
-public class Confiturerie {
+public class Confiturerie 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		
-
+         
 	}
 
 }
